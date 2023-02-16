d4048a0d23be8dfd4ed20fe704c27c9dd387f52f
1terator <kovalexnader13@gmail.com>
2023-02-16 09:55:04 +0300

    change ports for web

--- script.sh
+++ script.sh
@@ -3,4 +3,4 @@ python manage.py collectstatic --no-input
 echo "Run migrations"
 python manage.py migrate --no-input
 echo "Start server"
-gunicorn ecotextile.wsgi:application --bind 0.0.0.0:8000
+gunicorn ecotextile.wsgi:application --bind 0.0.0.0:7654
