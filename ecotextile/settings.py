52e7158acbc694fe690f92536f8fb72edaf78dc1
1terator <kovalexnader13@gmail.com>
2023-02-16 19:27:49 +0300

    add jwt to settings

--- ecotextile/settings.py
+++ ecotextile/settings.py
@@ -42,13 +42,18 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     "rest_framework",
     "django_celery_beat",
+    "rest_framework_simplejwt",
+    "auth",
     "document",
     "item",
 ]
 
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10
+    'PAGE_SIZE': 10,
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
 }
 
 MIDDLEWARE = [
