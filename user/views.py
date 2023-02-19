da8c4d8179916da855067ae3f0d5a4ea046ee5dc
1terator <kovalexnader13@gmail.com>
2023-02-19 17:50:57 +0300

    fix problem

--- user/views.py
+++ user/views.py
@@ -3,6 +3,7 @@ from rest_framework import generics, mixins
 from rest_framework.decorators import action
 from rest_framework.generics import get_object_or_404
 from rest_framework.response import Response
+from rest_framework.viewsets import GenericViewSet
 
 from .permissions import IsCurrentUser
 from .serializers import MyTokenObtainPairSerializer, RegisterSerializer, MiniUserSerializer
@@ -23,7 +24,7 @@ class RegisterView(mixins.CreateModelMixin, generics.GenericAPIView):
     serializer_class = RegisterSerializer
 
 
-class UserViewSet(generics.GenericAPIView):
+class UserViewSet(GenericViewSet):
     queryset = User.objects.all()
     permission_classes = (AllowAny, )
     serializer_class = MiniUserSerializer
