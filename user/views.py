e894926a8f9d1116d7b40541a3b2ef6a494f5d1c
1terator <kovalexnader13@gmail.com>
2023-02-19 22:58:00 +0300

    fix 405 problem

--- user/views.py
+++ user/views.py
@@ -18,7 +18,7 @@ class MyObtainTokenPairView(TokenObtainPairView):
     serializer_class = MyTokenObtainPairSerializer
 
 
-class RegisterView(mixins.CreateModelMixin, generics.GenericAPIView):
+class RegisterView(mixins.CreateModelMixin, GenericViewSet):
     queryset = User.objects.all()
     permission_classes = (AllowAny,)
     serializer_class = RegisterSerializer
